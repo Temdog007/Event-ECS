cd86a191d6c95a9cdb6ffd680b7a8902b256f250
Temdog007 <temdog007@yahoo.com>
2018-08-27 05:46:11 -0700

    Repalced custom UI components with Gspot

---
+++ Default Components/UI/guiHandlerComponent.lua
@@ -0,0 +1,66 @@
+local Component = require('drawableComponent')
+local class = require('classlib')
+local gui = require("Gspot")
+
+local guiHandlerComponent = class('guiHandlerComponent', Component)
+
+function guiHandlerComponent:__init(entity)
+  self:setDefault('name', classname(self))
+  self:set('entity', entity)
+end
+
+function guiHandlerComponent:eventLoad(args)
+  gui:load()
+end
+
+function guiHandlerComponent:eventUpdate(args)
+  if not args or not args.dt then return end
+
+  gui:update(args.dt)
+end
+
+function guiHandlerComponent:eventDraw(args)
+  if not self:canDraw(args) then return end
+
+  gui:draw()
+end
+
+function guiHandlerComponent:eventTextInput(args)
+  if not args or not args[1] then return end
+
+  if gui.focus then
+    gui:textinput(args[1])
+  end
+end
+
+function guiHandlerComponent:eventKeyPressed(args)
+  if not args or not args[1] or not args[2] or not args[3] then return end
+
+  if gui.focus then
+    gui:keypress(args[1])
+  else
+    gui:feedback(args[1])
+  end
+end
+
+function guiHandlerComponent:eventMousePressed(args)
+  if not args or not args[1] or not args[2] or not args[3] then return end
+
+  gui:mousepress(args[1], args[2], args[3])
+end
+
+function guiHandlerComponent:eventMouseReleased(args)
+  if not args or not args[1] or not args[2] or not args[3] then return end
+
+  gui:mouserelease(args[1], args[2], args[3])
+end
+
+function guiHandlerComponent:eventWheelMoved(args)
+  if not args or not args[1] or not args[2] then return end
+
+  gui:mousewheel(args[1], args[2])
+end
+
+lowerEventName(guiHandlerComponent)
+
+return guiHandlerComponent
