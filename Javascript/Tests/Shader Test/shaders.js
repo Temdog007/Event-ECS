df1e01fc07a1d9b46d35002a4eb7d7643f8c1b6b
Temdog007 <temdog007@yahoo.com>
2018-10-28 14:13:22 -0700

    Updated shader test

--- Javascript/Tests/Shader Test/shaders.js
+++ Javascript/Tests/Shader Test/shaders.js
@@ -54,77 +54,127 @@ define(function()
   `;
 
   var blur = `
+  precision mediump float;
+  varying vec2 vTextureCoord;
+  uniform sampler2D uSampler;
+  uniform vec2 direction;
+
+  void main()
+  {
+    vec4 c = vec4(0.0);
+
+    c += vec4(0.011109) * texture2D(uSampler, vTextureCoord + vec2(-30.000000) * direction);
+    c += vec4(0.014921) * texture2D(uSampler, vTextureCoord + vec2(-29.000000) * direction);
+    c += vec4(0.019841) * texture2D(uSampler, vTextureCoord + vec2(-28.000000) * direction);
+    c += vec4(0.026121) * texture2D(uSampler, vTextureCoord + vec2(-27.000000) * direction);
+    c += vec4(0.034047) * texture2D(uSampler, vTextureCoord + vec2(-26.000000) * direction);
+    c += vec4(0.043937) * texture2D(uSampler, vTextureCoord + vec2(-25.000000) * direction);
+    c += vec4(0.056135) * texture2D(uSampler, vTextureCoord + vec2(-24.000000) * direction);
+    c += vec4(0.071005) * texture2D(uSampler, vTextureCoord + vec2(-23.000000) * direction);
+    c += vec4(0.088922) * texture2D(uSampler, vTextureCoord + vec2(-22.000000) * direction);
+    c += vec4(0.110251) * texture2D(uSampler, vTextureCoord + vec2(-21.000000) * direction);
+    c += vec4(0.135335) * texture2D(uSampler, vTextureCoord + vec2(-20.000000) * direction);
+    c += vec4(0.164474) * texture2D(uSampler, vTextureCoord + vec2(-19.000000) * direction);
+    c += vec4(0.197899) * texture2D(uSampler, vTextureCoord + vec2(-18.000000) * direction);
+    c += vec4(0.235746) * texture2D(uSampler, vTextureCoord + vec2(-17.000000) * direction);
+    c += vec4(0.278037) * texture2D(uSampler, vTextureCoord + vec2(-16.000000) * direction);
+    c += vec4(0.324652) * texture2D(uSampler, vTextureCoord + vec2(-15.000000) * direction);
+    c += vec4(0.375311) * texture2D(uSampler, vTextureCoord + vec2(-14.000000) * direction);
+    c += vec4(0.429557) * texture2D(uSampler, vTextureCoord + vec2(-13.000000) * direction);
+    c += vec4(0.486752) * texture2D(uSampler, vTextureCoord + vec2(-12.000000) * direction);
+    c += vec4(0.546074) * texture2D(uSampler, vTextureCoord + vec2(-11.000000) * direction);
+    c += vec4(0.606531) * texture2D(uSampler, vTextureCoord + vec2(-10.000000) * direction);
+    c += vec4(0.666977) * texture2D(uSampler, vTextureCoord + vec2(-9.000000) * direction);
+    c += vec4(0.726149) * texture2D(uSampler, vTextureCoord + vec2(-8.000000) * direction);
+    c += vec4(0.782705) * texture2D(uSampler, vTextureCoord + vec2(-7.000000) * direction);
+    c += vec4(0.835270) * texture2D(uSampler, vTextureCoord + vec2(-6.000000) * direction);
+    c += vec4(0.882497) * texture2D(uSampler, vTextureCoord + vec2(-5.000000) * direction);
+    c += vec4(0.923116) * texture2D(uSampler, vTextureCoord + vec2(-4.000000) * direction);
+    c += vec4(0.955997) * texture2D(uSampler, vTextureCoord + vec2(-3.000000) * direction);
+    c += vec4(0.980199) * texture2D(uSampler, vTextureCoord + vec2(-2.000000) * direction);
+    c += vec4(0.995012) * texture2D(uSampler, vTextureCoord + vec2(-1.000000) * direction);
+    c += vec4(1.000000) * texture2D(uSampler, vTextureCoord + vec2(0.000000) * direction);
+    c += vec4(0.995012) * texture2D(uSampler, vTextureCoord + vec2(1.000000) * direction);
+    c += vec4(0.980199) * texture2D(uSampler, vTextureCoord + vec2(2.000000) * direction);
+    c += vec4(0.955997) * texture2D(uSampler, vTextureCoord + vec2(3.000000) * direction);
+    c += vec4(0.923116) * texture2D(uSampler, vTextureCoord + vec2(4.000000) * direction);
+    c += vec4(0.882497) * texture2D(uSampler, vTextureCoord + vec2(5.000000) * direction);
+    c += vec4(0.835270) * texture2D(uSampler, vTextureCoord + vec2(6.000000) * direction);
+    c += vec4(0.782705) * texture2D(uSampler, vTextureCoord + vec2(7.000000) * direction);
+    c += vec4(0.726149) * texture2D(uSampler, vTextureCoord + vec2(8.000000) * direction);
+    c += vec4(0.666977) * texture2D(uSampler, vTextureCoord + vec2(9.000000) * direction);
+    c += vec4(0.606531) * texture2D(uSampler, vTextureCoord + vec2(10.000000) * direction);
+    c += vec4(0.546074) * texture2D(uSampler, vTextureCoord + vec2(11.000000) * direction);
+    c += vec4(0.486752) * texture2D(uSampler, vTextureCoord + vec2(12.000000) * direction);
+    c += vec4(0.429557) * texture2D(uSampler, vTextureCoord + vec2(13.000000) * direction);
+    c += vec4(0.375311) * texture2D(uSampler, vTextureCoord + vec2(14.000000) * direction);
+    c += vec4(0.324652) * texture2D(uSampler, vTextureCoord + vec2(15.000000) * direction);
+    c += vec4(0.278037) * texture2D(uSampler, vTextureCoord + vec2(16.000000) * direction);
+    c += vec4(0.235746) * texture2D(uSampler, vTextureCoord + vec2(17.000000) * direction);
+    c += vec4(0.197899) * texture2D(uSampler, vTextureCoord + vec2(18.000000) * direction);
+    c += vec4(0.164474) * texture2D(uSampler, vTextureCoord + vec2(19.000000) * direction);
+    c += vec4(0.135335) * texture2D(uSampler, vTextureCoord + vec2(20.000000) * direction);
+    c += vec4(0.110251) * texture2D(uSampler, vTextureCoord + vec2(21.000000) * direction);
+    c += vec4(0.088922) * texture2D(uSampler, vTextureCoord + vec2(22.000000) * direction);
+    c += vec4(0.071005) * texture2D(uSampler, vTextureCoord + vec2(23.000000) * direction);
+    c += vec4(0.056135) * texture2D(uSampler, vTextureCoord + vec2(24.000000) * direction);
+    c += vec4(0.043937) * texture2D(uSampler, vTextureCoord + vec2(25.000000) * direction);
+    c += vec4(0.034047) * texture2D(uSampler, vTextureCoord + vec2(26.000000) * direction);
+    c += vec4(0.026121) * texture2D(uSampler, vTextureCoord + vec2(27.000000) * direction);
+    c += vec4(0.019841) * texture2D(uSampler, vTextureCoord + vec2(28.000000) * direction);
+    c += vec4(0.014921) * texture2D(uSampler, vTextureCoord + vec2(29.000000) * direction);
+    c += vec4(0.011109) * texture2D(uSampler, vTextureCoord + vec2(30.000000) * direction);
+    gl_FragColor = c * vec4(0.039985);
+  }
+  `;
+
+  var threshold = `
     precision mediump float;
     varying vec2 vTextureCoord;
     uniform sampler2D uSampler;
-    uniform vec2 direction;
-
+    uniform float min_luma;
     void main()
     {
-      vec4 c = vec4(0.0);
-  
-      c += vec4(0.011109) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.014921) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.019841) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.026121) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.034047) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.043937) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.056135) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.071005) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.088922) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.110251) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.135335) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.164474) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.197899) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.235746) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.278037) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.324652) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.375311) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.429557) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.486752) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.546074) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.606531) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.666977) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.726149) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.782705) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.835270) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.882497) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.923116) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.955997) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.980199) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.995012) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(1.000000) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.995012) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.980199) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.955997) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.923116) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.882497) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.835270) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.782705) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.726149) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.666977) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.606531) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.546074) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.486752) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.429557) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.375311) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.324652) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.278037) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.235746) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.197899) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.164474) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.135335) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.110251) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.088922) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.071005) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.056135) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.043937) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.034047) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.026121) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.019841) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.014921) * texture2D(uSampler, vTextureCoord * direction);
-      c += vec4(0.011109) * texture2D(uSampler, vTextureCoord * direction);
-      gl_FragColor = c * vec4(0.039985);
+      vec4 c = texture2D(uSampler, vTextureCoord);
+      float luma = dot(vec3(0.299, 0.587, 0.114), c.rgb);
+      gl_FragColor = c * step(min_luma, luma);
+    }
+  `;
+
+  var godsray = `
+    precision mediump float;
+    varying vec2 vTextureCoord;
+    uniform sampler2D uSampler;
+
+    uniform float exposure;
+    uniform float decay;
+    uniform float density;
+    uniform float weight;
+    uniform vec2 light_position;
+    uniform float samples;
+
+    const int max = 100;
+    void main()
+    {
+      vec2 uv = vTextureCoord;
+      vec4 color = texture2D(uSampler, uv); 
+      
+      vec2 offset = (uv - light_position) * density / samples;
+      float ill = decay;
+      vec4 c = vec4(0.0, 0.0, 0.0, 1.0);
+
+      for(int i = 0; i < max; ++i)
+      {
+        if(i > int(samples))
+        {
+          break;
+        }
+        uv -= offset;
+        c += texture2D(uSampler, uv) * ill * weight;
+        ill *= decay;
+      }
+
+      gl_FragColor = vec4(c.rgb * exposure + color.rgb, color.a);
     }
   `;
 
@@ -132,6 +182,8 @@ define(function()
     vert : vsSource,
     frag : fsSource,
     defFs : defFs,
-    blur : blur
+    blur : blur,
+    threshold : threshold,
+    godsray : godsray
   };
 });
--- Javascript/Tests/Shader Test/test.js
+++ Javascript/Tests/Shader Test/test.js
@@ -37,31 +37,52 @@ function(DrawableComponent, _, Systems, System, loadTexture, createSquare, shade
     return shader;
   }
 
-  var vert = loadShader(gl.VERTEX_SHADER, shaders.vert);
-  var frag = loadShader(gl.FRAGMENT_SHADER, shaders.blur);
+  function createProgram(vertShader)
+  {
+    var vert = loadShader(gl.VERTEX_SHADER, shaders.vert);
+    var frag = loadShader(gl.FRAGMENT_SHADER, vertShader);
 
-  var program = gl.createProgram();
-  gl.attachShader(program, vert);
-  gl.attachShader(program, frag);
-  gl.linkProgram(program);
-
-  if(!gl.getProgramParameter(program, gl.LINK_STATUS))
+    var program = gl.createProgram();
+    gl.attachShader(program, vert);
+    gl.attachShader(program, frag);
+    gl.linkProgram(program);
+    if(!gl.getProgramParameter(program, gl.LINK_STATUS))
   {
     console.log(gl.getProgramInfoLog(program));
   }
 
-  var programinfo ={
-    attribLocations: {
-      vertexPosition: gl.getAttribLocation(program, 'aVertexPosition'),
-      textureCoord: gl.getAttribLocation(program, 'aTextureCoord')
-    },
-    uniformLocations: {
-      projectionMatrix: gl.getUniformLocation(program, 'uProjectionMatrix'),
-      uSampler: gl.getUniformLocation(program, 'uSampler'),
-      oldColor : gl.getUniformLocation(program, 'oldColor'),
-      newColor : gl.getUniformLocation(program, 'newColor')
-    },
-  };
+    program.info ={
+      attribLocations: {
+        vertexPosition: gl.getAttribLocation(program, 'aVertexPosition'),
+        textureCoord: gl.getAttribLocation(program, 'aTextureCoord')
+      },
+      uniformLocations: {
+        projectionMatrix: gl.getUniformLocation(program, 'uProjectionMatrix'),
+        uSampler: gl.getUniformLocation(program, 'uSampler'),
+        oldColor : gl.getUniformLocation(program, 'oldColor'),
+        newColor : gl.getUniformLocation(program, 'newColor'),
+        direction : gl.getUniformLocation(program, 'direction'),
+        min_luma : gl.getUniformLocation(program, 'min_luma'),
+        exposure : gl.getUniformLocation(program, 'exposure'),
+        decay : gl.getUniformLocation(program, 'decay'),
+        density : gl.getUniformLocation(program, 'density'),
+        weight : gl.getUniformLocation(program, 'weight'),
+        samples : gl.getUniformLocation(program, 'samples'),
+        light_position : gl.getUniformLocation(program, 'light_position')
+      },
+    };
+
+    return program;
+  }
+
+  var programs = {};
+  for(var i in shaders)
+  {
+    if(i != "vert")
+    {
+      programs[i] = createProgram(shaders[i]);
+    }
+  }
 
   var buffer = createSquare(gl);
   var textureBuffer = createSquare(gl);
@@ -73,16 +94,23 @@ function(DrawableComponent, _, Systems, System, loadTexture, createSquare, shade
     bindTexture(canvas);
   }
 
-  // Shaders don't work with canvases
-  var testCanvas = document.createElement("canvas");
-  testCanvas.width = 64;
-  testCanvas.height = 64;
-  var testContext = testCanvas.getContext("2d");
-  testContext.fillStyle = "white";
-  testContext.fillRect(16, 16, 32, 32);
-  loadCanvas(testCanvas);
-  testCanvas.texture.oldColor = [0,0,0,1];
-  testCanvas.texture.newColor = [1,1,0,1];
+  function createTestCanvas(vertShader)
+  {
+    var testCanvas = document.createElement("canvas");
+    testCanvas.width = 64;
+    testCanvas.height = 64;
+    var testContext = testCanvas.getContext("2d");
+    testContext.fillStyle = "white";
+    testContext.fillRect(16, 16, 32, 32);
+    // testContext.fillRect(0, 0, 64, 64);
+    loadCanvas(testCanvas);
+    testCanvas.texture.width = 64;
+    testCanvas.texture.height = 64;
+    testCanvas.texture.oldColor = [0,0,0,1];
+    testCanvas.texture.newColor = [1,1,0,1];
+    testCanvas.texture.program = programs[vertShader];
+    return testCanvas;
+  }
 
   class Test extends DrawableComponent
   {
@@ -91,19 +119,58 @@ function(DrawableComponent, _, Systems, System, loadTexture, createSquare, shade
       super(entity);
       this.setDefaults({
         width : 100,
-        height : 100
+        height : 100,
+        min_luma : 0.2
       });
 
       // var texture = loadTexture(gl, "bombing blocks screenshot (6).png");
       this.texture = loadTexture(gl, "plunger.png");
       this.texture.oldColor = [1,0,1,1];
-      this.texture.newColor = [1,0,1,1];
+      this.texture.newColor = [0,1,1,1];
+      this.texture.program = programs.frag;
 
       this.texture2 = createTexture(gl);
       this.texture2.oldColor = [0,0,1,1];
       this.texture2.newColor = [0,1,1,1];
+      this.texture2.program = programs.defFs;
 
-      this.data.textures = [this.texture, this.texture2, testCanvas.texture];
+      this.data.textures = [this.texture, this.texture2];
+      this.data.textures.push(createTestCanvas("threshold").texture)
+      this.data.textures.push(createTestCanvas("blur").texture)
+      this.data.textures.push(createTestCanvas("blur").texture)
+      this.data.textures.push(createTestCanvas("frag").texture)
+      this.data.textures.push(createTestCanvas("blur").texture)
+      this.data.textures.push(createTestCanvas("blur").texture)
+      this.data.textures[3].dirWidth = true;
+      this.data.textures[4].dirhw = true;
+      this.data.textures[6].dirHeight = true;
+      this.data.textures[7].dirwh = true;
+      
+
+      this.data.textures.push(createTestCanvas("godsray").texture)
+      function set(t)
+      {
+        t.exposure = 0.25;
+        t.decay = 0.95;
+        t.density = 0.15;
+        t.weight = 0.5;
+        t.light_position = [0.5, 0.5];
+        t.samples = 70;
+        t.ray = true;
+      }
+      set(this.data.textures[8]);
+
+      this.data.positions = [
+        {x : 100, y : 100},
+        {x : 200, y : 200},
+        {x : 400, y : 300},
+        {x : 400, y : 300},
+        {x : 400, y : 300},
+        {x : 400, y : 300},
+        {x : 400, y : 300},
+        {x : 400, y : 300},
+        {x : 600, y : 400}
+      ]
     }
 
     eventUpdate(args)
@@ -114,35 +181,59 @@ function(DrawableComponent, _, Systems, System, loadTexture, createSquare, shade
         data.newColor[1] += args.dt * 0.001;
         data.newColor[1] %= 1;
       }
+      this.data.min_luma += (args.dt * 0.001) 
+      this.data.min_luma %= 1;
+
+      var tex = this.data.textures[8];
+      // tex.light_position[0] += args.dt * 0.001;
+      tex.light_position[1] += args.dt * 0.001;
+      // tex.light_position[0] %= 1;
+      tex.light_position[1] %= 1;
     }
 
     eventDraw()
     {
-      this.drawTexture(this.texture, 400, 100);
-      this.drawTexture(this.texture2, 100, 100);
-      this.drawTexture(testCanvas.texture, 300, 300);
+      this.context.save();
+      for(var i  = 0; i < this.data.textures.length; ++i)
+      {
+        var pos = this.data.positions[i];
+        if(i == 2)
+        {
+          this.context.globalCompositeOperation = "lighter";
+        }
+        if(i == 8)
+        {
+          this.context.globalCompositeOperation = "source-over";
+        }
+        this.drawTexture(this.data.textures[i], pos.x, pos.y);
+      }
+      this.context.restore();
     }
 
-    drawTexture(texture, x, y)
+    drawTexture(texture, x, y, c)
     {
+      c = c || canvas;
+
       var width = texture.width;
       var height = texture.height;
-      if(width > 0 && canvas.width !== width || height > 0 && canvas.height !== height)
+      if(width > 0 && c.width !== width || height > 0 && c.height !== height)
       {
-        canvas.width = width;
-        canvas.height = height;
+        c.width = width;
+        c.height = height;
       }
 
       var data = this.data;
 
-      gl.viewport(0, 0, canvas.width, canvas.height);
+      gl.viewport(0, 0, c.width, c.height);
 
       gl.clearColor(0.0, 0.0, 0.0, 1.0);
       gl.clear(gl.COLOR_BUFFER_BIT);
 
       gl.bindTexture(gl.TEXTURE_2D, texture);
 
+      var program = texture.program || programs.defFs;
       gl.useProgram(program);
+      var programinfo = program.info;
 
       gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
       gl.enableVertexAttribArray(programinfo.attribLocations.vertexPosition);
@@ -152,25 +243,53 @@ function(DrawableComponent, _, Systems, System, loadTexture, createSquare, shade
       gl.enableVertexAttribArray(programinfo.attribLocations.textureCoord);
       gl.vertexAttribPointer(programinfo.attribLocations.textureCoord, 2, gl.FLOAT, false, 0, 0);
 
-      var mat = mat4.orthographic(0, canvas.width, canvas.height, 0, -1, 1);
-      mat = mat4.scale(mat, canvas.width, canvas.height, 1);
+      var mat = mat4.orthographic(0, c.width, c.height, 0, -1, 1);
+      mat = mat4.scale(mat, c.width, c.height, 1);
 
       gl.uniformMatrix4fv(programinfo.uniformLocations.projectionMatrix, false, mat);
 
-      // gl.uniform4fv(programinfo.uniformLocations.newColor, texture.newColor);
-      // gl.uniform4fv(programinfo.uniformLocations.oldColor, texture.oldColor);
+      gl.uniform4fv(programinfo.uniformLocations.newColor, texture.newColor);
+      gl.uniform4fv(programinfo.uniformLocations.oldColor, texture.oldColor);
+      gl.uniform1f(programinfo.uniformLocations.min_luma, data.min_luma);
+      if(texture.dirWidth)
+      {
+        gl.uniform2fv(programinfo.uniformLocations.direction, [1 / width, 0]);
+      }
+      else if(texture.dirHeight)
+      {
+        gl.uniform2fv(programinfo.uniformLocations.direction, [0, 1/ height]);
+      }
+      else if(texture.dirwh)
+      {
+        return;
+        // gl.uniform2fv(programinfo.uniformLocations.direction, [1 / width, -1 / height]);
+      }
+      else if(texture.dirhw)
+      {
+        return;
+        // gl.uniform2fv(programinfo.uniformLocations.direction, [1 / width, 1 / height]);
+      }
+      else if(texture.ray)
+      {
+        gl.uniform1f(programinfo.uniformLocations.exposure, texture.exposure);
+        gl.uniform1f(programinfo.uniformLocations.decay, texture.decay);
+        gl.uniform1f(programinfo.uniformLocations.density, texture.density);
+        gl.uniform1f(programinfo.uniformLocations.weight, texture.weight);
+        gl.uniform1f(programinfo.uniformLocations.samples, texture.samples);
+        gl.uniform2fv(programinfo.uniformLocations.light_position, texture.light_position);
+      }
 
       gl.uniform1i(programinfo.uniformLocations.uSampler, 0);
 
-      gl.uniform2fv(programinfo.uniformLocations.direction, [1/32, 0]);
       gl.drawArrays(gl.TRIANGLES, 0, 6);
 
-      this.context.drawImage(canvas, 0, 0, canvas.width, canvas.height,
-        x, y, data.width, data.height);
+      this.context.drawImage(c, 0, 0, c.width, c.height,
+          x, y, data.width, data.height);
     }
+    
   }
 
   var system = Systems.addSystem(new System("Test"));
   var entity = system.createEntity();
-  var component = entity.addComponent(Test);
+  entity.addComponent(Test);
 });
