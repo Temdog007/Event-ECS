4e08d38ce176dac62c2ecee0b3cee6c99806b29d
Temdog007 <temdog007@yahoo.com>
2018-11-01 05:05:11 -0700

    Tested Extrude Geometry

--- Javascript/Tests/Three JS Test/test2.js
+++ Javascript/Tests/Three JS Test/test2.js
@@ -8,25 +8,45 @@ define(['DrawableComponent', 'Tests/Three JS Test/three'], function(Component, T
 
             var vertices = [
                 new THREE.Vector2(0, 0),
-                new THREE.Vector2(0, 1),
-                new THREE.Vector2(1, 1),
                 new THREE.Vector2(1, 0),
-            ]
+                new THREE.Vector2(1, 1),
+                new THREE.Vector2(0, 1),
+            ];
+            vertices.forEach(function(vertex)
+            {
+                vertex.x *= 10;
+                vertex.y *= 10;
+                vertex.x -= 0.5;
+                vertex.y -= 0.5;
+            })
 
-            var geometry = new THREE.ShapeGeometry(new THREE.Shape(vertices));
-            var material = new THREE.MeshBasicMaterial();
-    
-            this.mesh = new THREE.Mesh( geometry, material );
+            var shape = new THREE.Shape(vertices);
+            var geometry = new THREE.ShapeGeometry(shape);
+
+            this.mesh = new THREE.Mesh( geometry );
             this.mesh.position.x = 50;
             this.mesh.position.y = 50;
             this.time = 0;
             Component.Systems.pushEvent("eventAddMesh", this.mesh)
+
+            var comp = this;
+            var loader = new THREE.TextureLoader();
+            loader.load('ufo.png', function(image){
+                comp.mesh2 = new THREE.Mesh( new THREE.ExtrudeGeometry(shape, {depth : 10, bevelEnabled : false}), new THREE.MeshBasicMaterial({map : image}) );
+                comp.mesh2.position.x = -50;
+                comp.mesh2.position.y = -50;
+                Component.Systems.pushEvent("eventAddMesh", comp.mesh2)
+            });
         }
 
         eventUpdate(args)
         {
             this.time += args.dt * 0.001;
             this.mesh.position.x = (this.time % 1) * 50;
+
+            // if(!this.mesh2){return;}
+            // this.mesh2.rotation.x += args.dt * 0.001;
+            // this.mesh2.rotation.y += args.dt * 0.002;
         }
     }
 
